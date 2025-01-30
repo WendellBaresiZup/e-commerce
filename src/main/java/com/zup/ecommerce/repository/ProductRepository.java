150e754e2bfa7d540190a6e4a2042124ab803880
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-30 08:43:50 -0300

    Criado a camada repository, essa camada vai ser responsavel por gerenciar a comunicacao entre a aplicacao e o banco de dados. E dentro da camada foi criada a interface ProductRepository essa interface vai gerenciar um contrato para definir qual vai ser o metodo de acesso ao banco de dados

---
+++ src/main/java/com/zup/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.zup.ecommerce.repository;
+
+import com.zup.ecommerce.models.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Products, Long> {
+}
