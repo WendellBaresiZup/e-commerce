70de30d13b16517000c86867648999768c88e0a0
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-30 08:21:34 -0300

    Criado o metodo construtor da classe com todos os atributos e parametros. Criado o construtor vazio, esse construtor e um requesito do spring boot framework

--- src/main/java/com/zup/ecommerce/models/Products.java
+++ src/main/java/com/zup/ecommerce/models/Products.java
@@ -14,6 +14,17 @@ public class Products {
     private double price;
     private int quantity;
 
+    public Products(Long id, String name, double price, int quantity){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Products(){
+
+    }
+
     public Long getId() {
         return id;
     }
