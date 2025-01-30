16ada62df5be5b97f1a3a8032a6dbfc718cb647a
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-30 08:15:09 -0300

    Anotations implementadas dentro da classe

--- src/main/java/com/zup/ecommerce/models/Products.java
+++ src/main/java/com/zup/ecommerce/models/Products.java
@@ -1,7 +1,14 @@
 package com.zup.ecommerce.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class Products {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
     private Long id;
     private String name;
     private double price;
