bfb281a46da516455724fc9b51b37e82e0db33c5
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-30 10:24:47 -0300

    Implementado a anotation @RestController dentro da classe ProductController para informar ao spring que essa classe é a classe de controller

--- src/main/java/com/zup/ecommerce/controllers/ProductController.java
+++ src/main/java/com/zup/ecommerce/controllers/ProductController.java
@@ -3,13 +3,20 @@ package com.zup.ecommerce.controllers;
 import com.zup.ecommerce.models.Product;
 import com.zup.ecommerce.services.ProductService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/products")
 public class ProductController {
     private ProductService service;
 
     public ProductController(ProductService service) {
         this.service = service;
     }
+
 
     public ResponseEntity<Object> createProduct(Product product){
         ResponseEntity<Object> productCreated = service.createProduct(product);
