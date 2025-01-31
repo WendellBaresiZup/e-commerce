7ff3a1f9d7a82043924b7d3d5e17e167b8348a97
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-31 17:02:54 -0300

    Criado a classe CustomerService e injetando dentro dela a interface CustomerRepository

--- src/main/java/com/zup/ecommerce/services/CustomerService.java
+++ src/main/java/com/zup/ecommerce/services/CustomerService.java
@@ -1,4 +1,25 @@
 package com.zup.ecommerce.services;
 
+import com.zup.ecommerce.models.Customer;
+import com.zup.ecommerce.models.Product;
+import com.zup.ecommerce.repository.CustomerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
 public class CustomerService {
+
+    private final CustomerRepository repository;
+
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    public ResponseEntity<Map<String,Object>> createCustomer(Customer customer){
+        Customer created = new Customer(null, customer.getName(), customer.getCpf(), customer.getEmail());
+        Customer saveCustomer = repository.save(created);
+        Map<String, Object> bodyCustomer = Map.of("Message", "Customer Created Successfully", "Customer Data", new Customer(saveCustomer.getId(), saveCustomer.getName(), saveCustomer.getCpf(), saveCustomer.getEmail()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(bodyCustomer);
+    }
 }
