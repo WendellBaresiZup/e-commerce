43746c035bd6c910015307d2aa3c9f71e1d742d1
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-01 08:40:27 -0300

    Criado o metodo de cadastrar cliente na camada controller, nessa camada vai acontecer a relacao entre aplicacao e usuario para cadastrar o cliente

--- src/main/java/com/zup/ecommerce/controllers/CustomerController.java
+++ src/main/java/com/zup/ecommerce/controllers/CustomerController.java
@@ -1,6 +1,12 @@
 package com.zup.ecommerce.controllers;
 
+import com.zup.ecommerce.models.Customer;
 import com.zup.ecommerce.services.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 public class CustomerController {
 
@@ -10,5 +16,9 @@ public class CustomerController {
         this.service = service;
     }
 
-
+    @PostMapping
+    public ResponseEntity <Map<String,Object>> createCustomer(@RequestBody Customer customer){
+        ResponseEntity<Map<String, Object>> customerCreated = service.createCustomer(customer);
+        return ResponseEntity.status(customerCreated.getStatusCode()).body(customerCreated.getBody());
+    }
 }
