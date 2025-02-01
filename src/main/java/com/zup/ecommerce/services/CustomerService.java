3a80a2bc28be941a0f4b96ba10ab978296912d35
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-01 10:46:31 -0300

    Criado as validacoes para cadastrar um cliente

--- src/main/java/com/zup/ecommerce/services/CustomerService.java
+++ src/main/java/com/zup/ecommerce/services/CustomerService.java
@@ -5,11 +5,13 @@ import com.zup.ecommerce.models.Product;
 import com.zup.ecommerce.repository.CustomerRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-
+@Service
 public class CustomerService {
 
     private final CustomerRepository repository;
@@ -29,7 +31,7 @@ public class CustomerService {
         return repository.findAll().stream().map(customer -> new Customer(customer.getId(), customer.getName(), customer.getCpf(), customer.getEmail())).collect(Collectors.toList());
     }
 
-    public ResponseEntity<Map<String, Object>> updateProduct(Long id, Customer customerUpdate){
+    public ResponseEntity<Map<String, Object>> updateCustomer(Long id, Customer customerUpdate){
         Customer existingCustomer = repository.findById(id).orElseThrow(()-> new RuntimeException("Customer with ID " + id + " not found."));
         existingCustomer.setName(customerUpdate.getName());
         existingCustomer.setCpf(customerUpdate.getCpf());
@@ -38,6 +40,39 @@ public class CustomerService {
         Customer saveCustomer = repository.save(existingCustomer);
         Map<String, Object> bodyCustomer = Map.of("Message", "Customer Update Sucessfully", "New Customer Data", new Customer(saveCustomer.getId(), saveCustomer.getName(), saveCustomer.getCpf(), saveCustomer.getEmail()));
         return ResponseEntity.status(HttpStatus.OK).body(bodyCustomer);
+    }
+
+
+    private void validateCustomerName(String name){
+        if (name == null || name.isEmpty()){
+            throw new IllegalArgumentException("The Customer name cannot be empty!");
+        }
+    }
+
+    private void validateCustomerCpf(String cpf){
+        String cpfPattern = "^[0-9]{11}$";
+        if (cpf == null || !Pattern.matches(cpfPattern, cpf)){
+            throw new IllegalArgumentException("The Customer CPF must be a valid 11-digit number!");
+        }
+        if (repository.existsByCpf(cpf)){
+            throw new IllegalArgumentException("The Customer CPF already exists!");
+        }
+    }
+
+    private void validateCustomerEmail(String email){
+        String emailPattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-za-z]{2,6}$";
+        if (email == null || !Pattern.matches(emailPattern, email)){
+            throw new IllegalArgumentException("The Customer email must be valid!");
+        }
+        if (repository.existsByEmail(email)){
+            throw new IllegalArgumentException("The Customer email already exists!");
+        }
+    }
+
+    private void validateCustomer(Customer customer){
+        validateCustomerName(customer.getName());
+        validateCustomerCpf(customer.getCpf());
+        validateCustomerEmail(customer.getEmail());
     }
 
 }
