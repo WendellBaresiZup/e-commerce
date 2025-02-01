5610d01e8198fa11a4e567e2a9ce74f2a0bbfb1a
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-01 10:48:15 -0300

    Criado as validacoes existsByCpf e existsByEmail que vai modificar o nosso contrato para ter acesso ao banco de dados

--- src/main/java/com/zup/ecommerce/repository/CustomerRepository.java
+++ src/main/java/com/zup/ecommerce/repository/CustomerRepository.java
@@ -2,6 +2,10 @@ package com.zup.ecommerce.repository;
 
 import com.zup.ecommerce.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    boolean existsByCpf(String cpf);
+    boolean existsByEmail(String email);
 }
