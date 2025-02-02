800f0a9e2d363b0b57f06245462f69232d85cb51
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-02 13:25:01 -0300

    Implementado as anotations dentro da classe Customer, e implementado dentro do atributo id para ele ser auto incrementado

--- src/main/java/com/zup/ecommerce/models/Customer.java
+++ src/main/java/com/zup/ecommerce/models/Customer.java
@@ -1,6 +1,14 @@
 package com.zup.ecommerce.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String cpf;
