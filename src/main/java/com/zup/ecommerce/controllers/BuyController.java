2430da3f32faa746c60f480904c63313fbe9a79d
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-02 19:27:11 -0300

    Criado o metodo de comprar produto na camada controller, nessa camada vai acontecer a relacao entre aplicacao e usuario para atualizar o produto

--- src/main/java/com/zup/ecommerce/controllers/BuyController.java
+++ src/main/java/com/zup/ecommerce/controllers/BuyController.java
@@ -1,4 +1,25 @@
 package com.zup.ecommerce.controllers;
 
+import com.zup.ecommerce.services.BuyService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+@RestController
+@RequestMapping("/api/buy")
 public class BuyController {
+
+    private final BuyService buyservice;
+
+    public BuyController(BuyService buyservice) {
+        this.buyservice = buyservice;
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> buyProduct(@RequestBody Map<String, Object> buyRequest){
+        return buyservice.buyProduct(buyRequest);
+    }
 }
