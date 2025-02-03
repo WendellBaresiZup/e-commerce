e7a23c81e8ec546f55ca6e82761637b8367f9390
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-03 07:22:09 -0300

    Criado a classe BuyResponseDTO essa classe vai ser responsavel para enviar dados de volta do servidor para o cliente apos a requisicao. Dados da classe Buy

--- src/main/java/com/zup/ecommerce/dtos/BuyResponseDTO.java
+++ src/main/java/com/zup/ecommerce/dtos/BuyResponseDTO.java
@@ -1,4 +1,33 @@
 package com.zup.ecommerce.dtos;
 
+import java.util.List;
+
 public class BuyResponseDTO {
+    private String message;
+    private List<ProductResponseDTO> products;
+
+    public BuyResponseDTO(){
+
+    }
+
+    public BuyResponseDTO(String message, List<ProductResponseDTO> products) {
+        this.message = message;
+        this.products = products;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<ProductResponseDTO> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductResponseDTO> products) {
+        this.products = products;
+    }
 }
