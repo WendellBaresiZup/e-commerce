e9204c49a77e5df963d555af6ab119b5895ea4b5
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-03 07:20:45 -0300

    Criado a classe BuyRequestDTO essa classe vai ser responsavel para enviar dado de uma requisicao do cliente para o servidor. Dados da classe Buy

--- src/main/java/com/zup/ecommerce/dtos/BuyRequestDTO.java
+++ src/main/java/com/zup/ecommerce/dtos/BuyRequestDTO.java
@@ -1,4 +1,34 @@
 package com.zup.ecommerce.dtos;
 
+import java.util.List;
+
 public class BuyRequestDTO {
+
+    private String cpf;
+    private List<ProductRequestDTO> products;
+
+    public BuyRequestDTO(){
+
+    }
+
+    public BuyRequestDTO(String cpf, List<ProductRequestDTO> products) {
+        this.cpf = cpf;
+        this.products = products;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<ProductRequestDTO> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductRequestDTO> products) {
+        this.products = products;
+    }
 }
