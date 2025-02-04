d3c3d8f005f134fe88bc1581b064cb62dca99ecf
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-04 13:49:10 -0300

    Criado a classe de BuyNotFoundException para informar uma mensagem de erro se a compra nao for encontrada

--- src/main/java/com/zup/ecommerce/exceptions/BuyNotFoundException.java
+++ src/main/java/com/zup/ecommerce/exceptions/BuyNotFoundException.java
@@ -1,4 +1,7 @@
 package com.zup.ecommerce.exceptions;
 
-public class BuyNotFoundException {
+public class BuyNotFoundException extends RuntimeException{
+    public BuyNotFoundException(String message){
+        super(message);
+    }
 }
